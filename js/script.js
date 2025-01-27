d1aabbbe92835e20faf780fda9a6f96e345d5c4d
senzxc <devdxd084@gmail.com>
2025-01-27 12:21:51 +0700

    update script.js

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
 const jokeContainer = document.getElementById("jokes");
 const btn = document.getElementById("btn");
-const url = "https://candaan-api.vercel.app//api/text/random";
+const url = "https://candaan-api.vercel.app/api/text/random";
 
 let getJoke = () => {
     fetch(url)
     .then((response) => response.json())
     .then((result) => {
-        const jokes = result.data;
-        const randomJoke =jokes[Math.floor(Math.random() * jokes.length)];
-        jokeContainer.textContent = randomJoke;
+        jokeContainer.textContent = `${result.data}`;
     })
 };
 
